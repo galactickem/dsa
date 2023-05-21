74b2ffcd047fe9a8635b81a3dbc9d238c1aa581c
galactickem <a00827581@tec.mx>
2023-05-20 18:31:56 -0600

    LL pop method

--- Linked List/Exercises LL/EXERCISE-LL-Append.py
+++ Linked List/Exercises LL/EXERCISE-LL-Append.py
@@ -17,6 +17,11 @@ class LinkedList:
             print(temp.value)
             temp = temp.next
 
+    def make_empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
     def append(self, value):
         new_node = Node(value)
         if self.head == None:
@@ -29,5 +34,29 @@ class LinkedList:
         return True
 
 
-my_linked_list = LinkedList(4)
-print(my_linked_list.head.value)
+my_linked_list = LinkedList(1)
+my_linked_list.make_empty()
+
+my_linked_list.append(1)
+my_linked_list.append(2)
+
+print("Head:", my_linked_list.head.value)
+print("Tail:", my_linked_list.tail.value)
+print("Length:", my_linked_list.length, "\n")
+
+print("Linked List:")
+my_linked_list.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 1
+    Tail: 2
+    Length: 2 
+
+    Linked List:
+    1
+    2
+    
+"""
---
+++ Linked List/Exercises LL/EXERCISE-LL-Pop.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.head == None:
+            return None
+        if self.head.next == None:
+            temp = self.head
+            self.head = None
+            self.tail = None
+            return temp
+        temp = self.head
+        pre = self.head
+        while temp.next != None:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        return temp
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+
+# (2) Items - Returns 2 Node
+print(my_linked_list.pop().value)
+# (1) Item -  Returns 1 Node
+print(my_linked_list.pop().value)
+# (0) Items - Returns None
+print(my_linked_list.pop())
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    2
+    1
+    None
+
+"""
