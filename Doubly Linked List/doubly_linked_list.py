f553aaca1f4c5eadd3a70881bad4b00c51ac8c33
galactickem <a00827581@tec.mx>
2023-07-01 19:26:31 -0600

    append

--- Doubly Linked List/doubly_linked_list.py
+++ Doubly Linked List/doubly_linked_list.py
@@ -12,6 +12,12 @@ class DoublyLinkedList:
         self.tail = new_node
         self.length = 1
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+
 
 my_doubly_linked_list = DoublyLinkedList(7)
 my_doubly_linked_list = DoublyLinkedList(8)
